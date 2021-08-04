69f2bf818ebd3a91071a315cc04011082ef2bb6d
alfredo1995 <alfredogomes1995@gmail.com>
2021-08-04 18:00:58 -0300

    fasda

--- q5.js
+++ q5.js
@@ -1,15 +1,25 @@
-function fatorial() {
-    let inicio = new Date().getTime();
+let q5 = document.getElementById('q5');
 
-    let fat = document.getElementById('fatorial');
-    let number = Number(fat.value);
-    let result = 1;
+let CaixaTexto3 = document.createElement("input");  //   criar elemento <input></input>
+let CaixaTexto33 = document.createElement("input");  
+let CaixaTexto333 = document.createElement("input");  
+let botao5 = document.createElement("button");
 
-    for (i = 1; i <= number; i++) {
-        result = result * i;
-    }
 
-    let fim = new Date().getTime();
-    console.log(`${number}! = ${result} - durou ${fim - inicio} milisegundos`)
+CaixaTexto3.setAttribute("id", "CaixaTexto")  
+CaixaTexto3.setAttribute("type", "number")
+CaixaTexto3.setAttribute("placeholder", "lado a")
+CaixaTexto33.setAttribute("id", "CaixaTexto") 
+CaixaTexto33.setAttribute("type", "number")
+CaixaTexto33.setAttribute("placeholder", "lado b")
+CaixaTexto333.setAttribute("id", "CaixaTexto")  
+CaixaTexto333.setAttribute("type", "number")
+CaixaTexto333.setAttribute("placeholder", "lado c")
+botao5.setAttribute("id", 'button')
+botao5.innerHTML = "Calcular";
+botao5.setAttribute("type", "submit")
 
-}
+q5.appendChild(CaixaTexto3)  // recebendo parametro dos elemento de acima
+q5.appendChild(CaixaTexto33)
+q5.appendChild(CaixaTexto333)
+q5.appendChild(botao5)
