31c313488241a9465f8f21268e6b219c617a1671
alfredo1995 <alfredogomes1995@gmail.com>
2021-08-05 11:02:32 -0300

    atualizandoExercicio

--- q2.js
+++ q2.js
@@ -1,22 +1,22 @@
-let q2 = document.getElementById('q2');
+let q2 = document.getElementById('q2');                             // referenciando o documento HTML manipulado
  
-let CaixaTexto = document.createElement("input");  //   criar elemento <input></input>
+let CaixaTexto = document.createElement("input");                   // criando elemento <input></input>
 let botao2 = document.createElement("button"); 
  
-CaixaTexto.setAttribute("id", "CaixaTexto")  //  atributo do elemento <input id="id" name="CaixaTexto"></input>
-CaixaTexto.setAttribute("type", "number") 
+CaixaTexto.setAttribute("id", "CaixaTexto")                         //  atributo do elemento <input id="id" name="CaixaTexto"></input>
+CaixaTexto.setAttribute("type", "number")                           //  atributo do elemento <input type="number" id="id" name="CaixaTexto"/>></input>
 botao2.setAttribute("id", 'button')
 botao2.innerHTML = "Calcular";
 botao2.setAttribute("type", "submit")  
- 
-q2.appendChild(CaixaTexto)  // recebendo parametro dos elemento de acima
+    
+q2.appendChild(CaixaTexto)                                          // recebendo os elementos como parametro para instaciar na tela
 q2.appendChild(botao2)
  
-let button2 = document.getElementById('button');  // manipulando o elemento
- 
-function fatorial() {
- 
-    button2.addEventListener('click', function () { fatorial() }); //    ao clicar no elemnto aciona a função
+let button2 = document.getElementById('button');                    // manipulando o elemento
+
+button2.addEventListener('click', function () { fatorial() });      // ao clicar no elemnto aciona a função
+
+function fatorial() {                                               // function aciona ao clicar no button2.addEventListener
  
     let fatorial = document.getElementById('CaixaTexto').value;
     let inicio = new Date().getTime();
--- q6.js
+++ q6.js
@@ -1,1 +1,6 @@
 let q6 = document.getElementById('q6');
+	q6.innerHTML = ' <h1>login</h1> 	<div id="nao_logado"> <input id="username" type="text"> '
+	+ '<input id="password" type="password"> <button onclick="logar()">Login</button>'
+	+ '<br/><br/><div>Novo Usuario</div> <input id="new_user" type="text"> <input id="new_password" type="password">'
+	+ '<button onclick="criarUsuarioNovo()">Criar</button>	</div> <div id="messages"></div>	<div id="logado" style="visibility:hidden"> <div>Logado</div>'
+	+ '<button onclick="deslogar()">Deslogar</button> </div> <script src="q6.js"> 	</script> </body> </html>';
